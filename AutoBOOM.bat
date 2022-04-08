6851f0a77ad1517d2604d50df49b7d39cab71887
r8928 <tmp.rashid@gmail.com>
2022-04-08 16:27:04 -0700

    Reporting email

--- AutoBOOM.bat
+++ AutoBOOM.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
+rem PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
 
-REM PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
--- parameters.ps1
+++ parameters.ps1
@@ -22,7 +22,7 @@ $paths = @{
   boom_r_err_filename = "autopull_errors.log"
 
   error_email_url     = "https://boom.cellshop.us/api/v1/send_mail"
-  email_sendto        = "tmp.rashid@gmail.com"
+  email_sendto        = "tmp.rashid@gmail.com,reporting@cellshop.us"
 
 }
 
